050448146954f81e0149c63414a235fda8a5c9c0
Jeremy Abulencia <Jeremy Abulencia@egg.ph>
2016-09-02 15:44:22 +0800

    maskmod update

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
 	maskContWidth = $('.mask .cont').width()/2;
-	$('.mask .cont').css({'margin-left':'-'+maskContWidth+'px'})
+	maskMod(maskContWidth);
 })
+$(window).resize(function(){
+	maskContWidth = $('.mask .cont').width()/2;
+	maskMod(maskContWidth);
+})
+function maskMod(maskContWidth){
+	$('.mask .cont').css({'margin-left':'-'+maskContWidth+'px'})
+}
