fb21aa92208262ec59f0ea1b2708036c8271280f
Jeremy Abulencia <Jeremy Abulencia@egg.ph>
2016-09-05 10:42:30 +0800

    update mask;script links on socs

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ $(window).resize(function(){
 	maskContWidth = $('.mask .cont').width()/2;
 	maskMod(maskContWidth);
 })
+$('#socialPanel ul li').click(function(){
+	href = $(this).find('a').data('href');
+	window.open(href);
+})
+$('.mask .close').click(function(){
+	$('.mask').addClass('hide');
+})
 function maskMod(maskContWidth){
 	$('.mask .cont').css({'margin-left':'-'+maskContWidth+'px'})
 }
